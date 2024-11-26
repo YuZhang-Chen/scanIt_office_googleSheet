264bc663b5e376626e39d68ef734e96b6077b40b
CHEN,YU-ZHANG <c112156205@nkust.edu.tw>
2024-11-26 19:38:57 +0800

    20241126

--- front/getData.js
+++ front/getData.js
@@ -1,3 +1,5 @@
+// 刷新頁面時，如果data_log.json裡有之前的資料，就先將它們顯示出來
+
 async function getData() {
     try {
         const response = await fetch('../server/data_log.json');
@@ -6,9 +8,13 @@ async function getData() {
         data.forEach(element => {                  
             const target = `
             <div class="student-entry">
+            <div class="info">
+                <span class="student-title">${element['title']}</span>
                 <span class="student-name">${element['name']}</span>
-                <span class="timestamp">${element['time']}</span>
-            </div><br>`
+            </div>
+            <span class="timestamp">${element['time']}</span>
+        </div>
+        <br>`
             document.getElementById('content').innerHTML += target;
         });
     } catch (error) {
